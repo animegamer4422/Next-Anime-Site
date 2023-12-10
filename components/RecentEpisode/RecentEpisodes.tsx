c745f769a448741ab5dc04a6db0e6ac12441d1e9
DeadPool-4422 <dynamicplayer444@gmail.com>
2023-12-10 17:18:17 +0530

    Play the recent episode instead of opening details

--- components/RecentEpisode/RecentEpisodes.tsx
+++ components/RecentEpisode/RecentEpisodes.tsx
@@ -1,11 +1,12 @@
 import { useEffect, useState } from 'react';
-import Link from 'next/link';
-import style from "./RecentAnime.module.css"
+import style from "./RecentAnime.module.css";
+
 interface Episode {
   id: string;
   image: string;
   title: string;
   episodeNumber: number;
+  url: string;
 }
 
 export default function RecentEpisodes() {
@@ -22,14 +23,20 @@ export default function RecentEpisodes() {
       let response = await fetch('https://animetrix-api.vercel.app/anime/gogoanime/recent-episodes');
       if (!response.ok) throw new Error('Error with first API');
       const data = await response.json();
-      setRecentEpisodes(data.results);
+      setRecentEpisodes(data.results.map((episode: Episode) => ({
+        ...episode,
+        url: `http://localhost:3000/video-player/${episode.episodeNumber}/${episode.title.replace(/\s+/g, '-').toLowerCase()}-episode-${episode.episodeNumber}`
+      })));
     } catch (error) {
       console.log('First API failed, trying with fallback API');
       try {
         const responseFallback = await fetch('https://api.consumet.org/anime/gogoanime/recent-episodes');
         if (!responseFallback.ok) throw new Error('Error with fallback API');
         const dataFallback = await responseFallback.json();
-        setRecentEpisodes(dataFallback.results);
+        setRecentEpisodes(dataFallback.results.map((episode: Episode) => ({
+          ...episode,
+          url: `http://localhost:3000/video-player/${episode.episodeNumber}/${episode.title.replace(/\s+/g, '-').toLowerCase()}-episode-${episode.episodeNumber}`
+        })));
       } catch (fallbackError) {
         console.error('Fallback API also failed', fallbackError);
         setRecentEpisodes([]);
@@ -44,16 +51,15 @@ export default function RecentEpisodes() {
       <h1 className="text-2xl font-bold mb-4">New Releases</h1>
       {loading && <div className={style.loader}></div>}
       <div className="grid">
-        {recentEpisodes.map(episode => (
-          <Link key={episode.id} href={`/anime/${episode.id}`}>
+        {recentEpisodes.map((episode: Episode) => (
+          <a key={episode.id} href={episode.url}>
             <div className={style.recentAnimeImage}>
               <div className={style.recentAnimeImg} style={{backgroundImage: `url(${episode.image})`}} />
               <h2 className={style.recentAnimeName}>{episode.title}</h2>
             </div>
-          </Link>
+          </a>
         ))}
       </div>
     </div>
-  ); 
-    
+  );
 }
