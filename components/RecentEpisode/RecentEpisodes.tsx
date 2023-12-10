7c5082f92af053592f9155fd7f77b4fc0c7081ca
DeadPool-4422 <dynamicplayer444@gmail.com>
2023-12-10 18:42:24 +0530

    Refactoring of video player and Recent Episodes

--- components/RecentEpisode/RecentEpisodes.tsx
+++ components/RecentEpisode/RecentEpisodes.tsx
@@ -1,3 +1,4 @@
+import { useRouter } from 'next/router';
 import { useEffect, useState } from 'react';
 import style from "./RecentAnime.module.css";
 
@@ -6,12 +7,18 @@ interface Episode {
   image: string;
   title: string;
   episodeNumber: number;
-  url: string;
+  episodeId: string;
 }
 
 export default function RecentEpisodes() {
   const [recentEpisodes, setRecentEpisodes] = useState<Episode[]>([]);
   const [loading, setLoading] = useState(false);
+  const router = useRouter(); // Initialize useRouter
+
+  // Function to navigate to the video player
+  const navigateToVideoPlayer = (episodeId: string) => {
+    router.push(`/video-player/${episodeId}`);
+};
 
   useEffect(() => {
     fetchRecentEpisodes();
@@ -23,20 +30,14 @@ export default function RecentEpisodes() {
       let response = await fetch('https://animetrix-api.vercel.app/anime/gogoanime/recent-episodes');
       if (!response.ok) throw new Error('Error with first API');
       const data = await response.json();
-      setRecentEpisodes(data.results.map((episode: Episode) => ({
-        ...episode,
-        url: `http://localhost:3000/video-player/${episode.episodeNumber}/${episode.title.replace(/\s+/g, '-').toLowerCase()}-episode-${episode.episodeNumber}`
-      })));
+      setRecentEpisodes(data.results); // Set the episodes directly without modifying the URL
     } catch (error) {
       console.log('First API failed, trying with fallback API');
       try {
         const responseFallback = await fetch('https://api.consumet.org/anime/gogoanime/recent-episodes');
         if (!responseFallback.ok) throw new Error('Error with fallback API');
         const dataFallback = await responseFallback.json();
-        setRecentEpisodes(dataFallback.results.map((episode: Episode) => ({
-          ...episode,
-          url: `http://localhost:3000/video-player/${episode.episodeNumber}/${episode.title.replace(/\s+/g, '-').toLowerCase()}-episode-${episode.episodeNumber}`
-        })));
+        setRecentEpisodes(dataFallback.results); // Set the episodes directly from the fallback API
       } catch (fallbackError) {
         console.error('Fallback API also failed', fallbackError);
         setRecentEpisodes([]);
@@ -44,20 +45,23 @@ export default function RecentEpisodes() {
     } finally {
       setLoading(false);
     }
-  }
+  }  
 
-  return (
+return (
     <div className="container">
       <h1 className="text-2xl font-bold mb-4">New Releases</h1>
       {loading && <div className={style.loader}></div>}
       <div className="grid">
-        {recentEpisodes.map((episode: Episode) => (
-          <a key={episode.id} href={episode.url}>
-            <div className={style.recentAnimeImage}>
-              <div className={style.recentAnimeImg} style={{backgroundImage: `url(${episode.image})`}} />
-              <h2 className={style.recentAnimeName}>{episode.title}</h2>
-            </div>
-          </a>
+        {recentEpisodes.map((episode) => (
+          <div 
+            key={episode.id} 
+            className={style.recentAnimeImage} 
+            onClick={() => navigateToVideoPlayer(episode.episodeId)}
+            style={{cursor: 'pointer'}}
+          >
+            <div className={style.recentAnimeImg} style={{backgroundImage: `url(${episode.image})`}} />
+            <h2 className={style.recentAnimeName}>{episode.title}</h2>
+          </div>
         ))}
       </div>
     </div>
---
+++ pages/video-player/[episodeId].tsx
@@ -0,0 +1,107 @@
+import { useRouter } from 'next/router';
+import { useEffect, useState, useRef } from 'react';
+import Plyr from 'plyr';
+import 'plyr/dist/plyr.css';
+import Hls from 'hls.js';
+import '../../src/app/globals.css';
+import './Episode.module.css';
+import Head from 'next/head';
+import Header from '../../components/Header/Header';
+import AnimeSearch from '../../components/AnimeSearch/AnimeSearch';
+import Footer from '../../components/Footer/Footer';
+
+interface Anime {
+  title: string;
+  episode: string;
+}
+
+export default function VideoPlayer() {
+  const router = useRouter();
+  const { episodeId } = router.query;
+  const videoRef = useRef<HTMLVideoElement>(null);
+  const [anime, setAnime] = useState<Anime | null>(null);
+  const [mainUrl, setMainUrl] = useState("");
+
+  useEffect(() => {
+    if (episodeId && typeof episodeId === 'string') {
+      const apiUrl = `https://api.amvstr.me/api/v2/stream/${episodeId}`;
+      console.log("Fetching stream from:", apiUrl);
+
+      fetch(apiUrl)
+        .then(response => response.json())
+        .then(data => {
+          const mainUrl = data.stream.multi.main.url;
+          const episodeNumber = episodeId.split('-').pop();
+          const animeTitle = episodeId.split('-').slice(0, -1).join(' ');
+
+          setMainUrl(mainUrl);
+          setAnime({ title: animeTitle, episode: episodeNumber });
+        })
+        .catch(error => console.error('Error fetching stream:', error));
+    }
+  }, [episodeId]);
+
+  useEffect(() => {
+    if (mainUrl && videoRef.current) {
+      const video = videoRef.current;
+      const player = new Plyr(video, {
+        controls: [
+          "play-large", "rewind", "play", "fast-forward", "progress",
+          "current-time", "duration", "mute", "volume", "captions",
+          "settings", "pip", "airplay", "fullscreen",
+        ],
+      });
+
+      if (Hls.isSupported()) {
+        const hls = new Hls();
+        hls.loadSource(mainUrl);
+        hls.attachMedia(video);
+        hls.on(Hls.Events.MANIFEST_PARSED, () => video.pause());
+      } else if (video.canPlayType("application/vnd.apple.mpegurl")) {
+        video.src = mainUrl;
+        video.addEventListener("canplaythrough", () => video.pause());
+      } else {
+        console.error("This is a legacy browser that does not support HLS.");
+      }
+    }
+  }, [mainUrl]);
+
+  return (
+    <div>
+      <Head>
+        <title>{anime ? `${anime.title} - Episode ${anime.episode}` : 'Loading...'} | Anime Site</title>
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" />
+        <link rel="stylesheet" href="https://cdn.plyr.io/3.6.9/plyr.css" />
+        <style>{`
+          :root {
+            --plyr-color-main: #ff69b4;
+            --plyr-video-background: #282a36;
+          }
+        `}</style>
+      </Head>
+
+      <Header />
+      <div className="search-section">
+        <AnimeSearch />
+      </div>
+
+      <main>
+        <section className="video-section">
+          <div className="container">
+          <h2 className="current-ep">Current Episode - {anime?.episode}</h2>
+            <div className="player-wrapper">
+              {mainUrl && (
+                <video ref={videoRef} id="player" width="320" height="240" controls playsInline>
+                  <source src={mainUrl} type="application/vnd.apple.mpegurl" />
+                  Your browser does not support the video tag.
+                </video>
+              )}
+            </div>
+          </div>
+        </section>
+      </main>
+
+      <Footer />
+    </div>
+  );
+}
