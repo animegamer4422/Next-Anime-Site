5a333741734b4d324bc089a1af5663be02f2d0a4
DeadPool-4422 <dynamicplayer444@gmail.com>
2023-12-25 20:14:16 +0530

    modularised the Fetching of Anime Details

--- pages/anime/[animeId].tsx
+++ pages/anime/[animeId].tsx
@@ -1,30 +1,15 @@
+import React, { useState } from 'react';
 import { useRouter } from 'next/router';
-import { SetStateAction, useEffect, useState } from 'react';
 import Link from 'next/link';
 import AnimeSearch from '../../components/AnimeSearch/AnimeSearch';
-
-import '../../src/app/globals.css';
-import styles from './AnimeDetails.module.css'
 import Header from '../../components/Header/Header';
-
-interface Anime {
-  id: string;
-  title: string;
-  image: string;
-  description: string;
-  episodes: Episode[];
-}
-
-interface Episode {
-  number: number;
-  id: string;
-}
+import styles from './AnimeDetails.module.css';
+import useFetchAnimeDetails from './useFetchAnimeDetails'; // Import the custom hook
 
 export default function AnimeDetails() {
-  const [anime, setAnime] = useState<Anime | null>(null);
   const router = useRouter();
   const { animeId } = router.query;
-  const [loading, setLoading] = useState<boolean>(true);
+  const { anime, loading, error } = useFetchAnimeDetails(animeId); // Use the custom hook
   const [searchTerm, setSearchTerm] = useState('');
 
   const handleSearch = (event: React.ChangeEvent<HTMLInputElement>) => {
@@ -35,97 +20,57 @@ export default function AnimeDetails() {
     episode.number.toString().includes(searchTerm)
   ) : [];
 
-  useEffect(() => {
-    const fetchAnimeDetails = async () => {
-      setLoading(true);
-      if (typeof animeId === 'undefined') return;
-
-      const primaryApiUrl = `https://api-consumet-org-six.vercel.app/anime/gogoanime/info/${animeId}`;
-      const fallbackUrl = `https://api.consumet.org/anime/gogoanime/info/${animeId}`;
-
-      try {
-        const response = await fetch(primaryApiUrl);
-        console.log(primaryApiUrl);
-
-        if (!response.ok) {
-          throw new Error('Error fetching data from primary API');
-        }
-
-        const data = await response.json();
-        setAnime(data);
-      } catch (primaryApiError) {
-        console.error('Error:', primaryApiError);
-      
-        try {
-          const fallbackResponse = await fetch(fallbackUrl);
-
-          if (!fallbackResponse.ok) {
-            throw new Error('Error fetching data from fallback API');
-          }
-
-          const fallbackData = await fallbackResponse.json();
-          setAnime(fallbackData);
-        } catch (fallbackApiError) {
-          console.error('Error:', fallbackApiError);
-        }
-      }
-      setLoading(false);
-    };
-
-    fetchAnimeDetails();
-  }, [animeId]);
-
   return (
     <>
       <Header/>
       <form id="search-form">
-        <AnimeSearch />
+        <AnimeSearch setQuery={function (query: string): void {
+          throw new Error('Function not implemented.');
+        } } />
       </form>
-    
+
       <div className={styles.content}>
-        <div className={styles.animeContainer}>
-          {loading ? (
-            <div className={styles.loader}></div>
-          ) : anime ? (
-            <div className={styles.animeContainer}>
-              <div className={styles.animeDetails}>
-                <h1 id="anime-title">{anime.title}</h1>
-                <div id="anime-image">
-                  <img src={anime.image} alt={anime.title} />
-                </div>
-                <p className={styles.animeDesc}> {anime.description}</p>
+        {loading ? (
+          <div className={styles.loader}></div>
+        ) : error ? (
+          <p>Error loading data: {error}</p>
+        ) : anime ? (
+          <div className={styles.animeContainer}>
+            <div className={styles.animeDetails}>
+              <h1 id="anime-title">{anime.title}</h1>
+              <div id="anime-image">
+                <img src={anime.image} alt={anime.title} />
               </div>
-    
-              <div className={styles.episodesSection}>
-                <div className={styles.episodeSearchSection}>
-                  <form className={styles.episodeSearchForm}>
-                    <input 
-                      type="text" 
-                      placeholder="Search episode..." 
-                      value={searchTerm} 
-                      onChange={handleSearch} 
-                      className={styles.episodeSearchInput}
-                    />
-                  </form>
-                </div>
-                <ul id="anime-episodes" className={styles.episodeGrid}>
-                  {filteredEpisodes.map((episode) => (
-                    <li key={episode.id}>
-                      <Link href={`/video-player/${episode.id}`}>
-                          <h2 className={styles.episodeBtn}>
-                            EP {episode.number}
-                          </h2>
-                      </Link>
-                    </li>
-                  ))}
-                </ul>
-              </div>
+              <p className={styles.animeDesc}> {anime.description}</p>
             </div>
-          ) : (
-            <p>Error loading data...</p>
-          )}
-        </div>
+
+            <div className={styles.episodesSection}>
+              <div className={styles.episodeSearchSection}>
+                <input 
+                  type="text" 
+                  placeholder="Search episode..." 
+                  value={searchTerm} 
+                  onChange={handleSearch} 
+                  className={styles.episodeSearchInput}
+                />
+              </div>
+              <ul id="anime-episodes" className={styles.episodeGrid}>
+                {filteredEpisodes.map((episode) => (
+                  <li key={episode.id}>
+                    <Link href={`/video-player/${episode.id}`}>
+                      <h2 className={styles.episodeBtn}>
+                        EP {episode.number}
+                      </h2>
+                    </Link>
+                  </li>
+                ))}
+              </ul>
+            </div>
+          </div>
+        ) : (
+          <p>Anime not found.</p>
+        )}
       </div>
     </>
-  );  
+  );
 }
---
+++ pages/anime/useFetchAnimeDetails.ts
@@ -0,0 +1,55 @@
+// useFetchAnimeDetails.ts
+import { useState, useEffect } from 'react';
+
+// Interfaces for Anime details
+interface Anime {
+  id: string;
+  title: string;
+  image: string;
+  description: string;
+  episodes: Episode[];
+}
+
+interface Episode {
+  number: number;
+  id: string;
+}
+
+const useFetchAnimeDetails = (animeId: string | string[] | undefined) => {
+  const [anime, setAnime] = useState<Anime | null>(null);
+  const [loading, setLoading] = useState<boolean>(true);
+  const [error, setError] = useState<string>('');
+
+  useEffect(() => {
+    const fetchAnimeDetails = async () => {
+      if (!animeId || typeof animeId !== 'string') return;
+
+      setLoading(true);
+      setError('');
+
+      const apiUrl = `https://api-consumet-org-six.vercel.app/anime/gogoanime/info/${animeId}`;
+      try {
+        const response = await fetch(apiUrl);
+        if (!response.ok) throw new Error('Failed to fetch');
+
+        const data = await response.json() as Anime;
+        setAnime(data);
+      } catch (err) {
+        // Type check for the error object
+        if (err instanceof Error) {
+          setError(err.message);
+        } else {
+          setError('An unknown error occurred');
+        }
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchAnimeDetails();
+  }, [animeId]);
+
+  return { anime, loading, error };
+};
+
+export default useFetchAnimeDetails;
